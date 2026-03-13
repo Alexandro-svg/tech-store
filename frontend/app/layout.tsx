dbe8839b6abeb7099f6bb2580fb8389431b38713
Maxim Musatov <sldpsnow8800@gmail.com>
2026-03-13 20:11:52 +0200

    пепе фа

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <b className="text-5xl">ЖОПАААААААААА</b>
-        <p>я сосал меня ебали</p>
-        {/* {children} */}
+        <Navbar />
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
