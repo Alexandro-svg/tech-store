e1a86741fb5d96d86c33e6189e6862dfae1f3826
Alexandro-svg <zyxel6565@gmail.com>
2026-03-19 18:48:49 +0200

    что сделал

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,8 +3,16 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
